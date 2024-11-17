ac7e9dd297b118d93587c769b7b519fe322197a7
scientivan <69758590+scientivan@users.noreply.github.com>
2024-11-17 09:10:59 +0700

    Add files via upload

--- all.js
+++ all.js
@@ -4,3 +4,15 @@ function DropDown(){
     // alert('tes');
     menu.classList.toggle('nav-menu-open');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const portfolioButton = document.getElementById('portfolio-button');
+    const portfolioDropdown = document.getElementById('portfolio-dropdown');
+
+    // Toggle dropdown visibility when button is clicked
+    portfolioButton.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent default behavior of anchor tag
+        portfolioDropdown.classList.toggle('active');
+    });
+});
+
